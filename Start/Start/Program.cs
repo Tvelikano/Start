d69f0e5d70421483278fce1c32a698c045b23120
AnyLogarithm <timvel.1999@gmail.com>
2018-11-20 15:06:13 +0300

    1.0.1

--- Start/Start/Program.cs
+++ Start/Start/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace Start
@@ -13,30 +14,46 @@ namespace Start
             XmlSerializer serializer = new XmlSerializer(typeof(RecordList));
             RecordList records = new RecordList();
 
-            
+            ReadRecords(ref records, serializer);
+            WriteRecord(records, serializer);
+
+            Console.ReadKey();
+        }
+
+        static void ReadRecords(ref RecordList records, XmlSerializer serializer)
+        {
             using (FileStream fs = new FileStream("records.xml", FileMode.OpenOrCreate))
             {
-                records = (RecordList)serializer.Deserialize(fs);
-                
-                foreach (Record rec in records.list.OrderByDescending(rc => rc.CreateDate).Take(3))
+                try
                 {
-                    Console.WriteLine($"{rec.Head}\n{rec.Content}\nДата создания записи {rec.CreateDate}\n");
+                    records = (RecordList) serializer.Deserialize(fs);
+                    Console.WriteLine($"Всего записей: {records.list.Count()}");
+
+                    foreach (Record rec in records.list.OrderByDescending(rc => rc.CreateDate).Take(3))
+                    {
+                        Console.WriteLine($"{rec.Head}\nДата создания записи: {rec.CreateDate}\n");
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("В списке нет записей");
                 }
             }
+        }
 
+        static void WriteRecord(RecordList records, XmlSerializer serializer)
+        {
             using (FileStream fs = new FileStream("records.xml", FileMode.OpenOrCreate))
             {
-                Console.WriteLine("Введите заголовок записи");
+                Console.WriteLine("Добавить новую запись\nВведите заголовок записи:");
                 string head = Console.ReadLine();
-                Console.WriteLine("Введите содержимое записи");
+                Console.WriteLine("Введите содержимое записи:");
                 string content = Console.ReadLine();
                 Record newRecord = new Record(head, content);
                 records.list.Add(newRecord);
                 serializer.Serialize(fs, records);
                 Console.WriteLine("Запись добавлена");
             }
-
-            Console.ReadKey();
         }
     }
 }
